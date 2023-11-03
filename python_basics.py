6401efb0cdd70030b152f33889bd47b31f96cf6f
nayan reddy <nayanads10@gmail.com>
2023-11-03 07:48:26 +0530

    2/11/2023 class

---
+++ python_basics.py
@@ -0,0 +1,55 @@
+# single line commenting-it can be done by pressing shift+3 key
+
+# single line commenting-it can be done by pressing ctrl+? and if you press ctrl+? on commented line its gets uncommented
+
+"""
+multiple line commenting - it is enclosed between triplet "
+Welcome to python class
+basics of python
+theory of python
+"""
+
+# assignment operator
+
+a = 5                                      # a is variable and 5 is value of variable a
+print(a)
+print("The value of a is",a)
+print("The value of a:{}".format(a))
+
+b = 15
+print("The value of a is {} and the value of b is {}".format(a,b))
+
+a = 20
+print("The value of a is after replacing {}".format(a))
+
+# sum of a and b
+sum_a_b=a+b
+print("The sum of a and b is {}".format(sum_a_b))
+
+# difference od a and b
+diff_a_b= a-b
+print("The difference of a and b is {}".format(diff_a_b))
+
+# product of a and b
+prod_a_b= a*b
+print("The product of a and b is {}".format(prod_a_b))
+
+# to get quotient
+quot_a_b= a/b
+print("The quotient when a is divided by b is {}".format(quot_a_b))
+
+# to get remainder
+rem_a_b= a%b
+print("The remainder when a is divided by b {}".format(rem_a_b))
+
+
+
+
+
+
+
+
+
+
+
+

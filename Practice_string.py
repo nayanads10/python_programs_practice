f61ab89bff2fdea0abdcbe1a57b1e326e3ddeb35
nayan reddy <nayanads10@gmail.com>
2023-11-07 07:33:23 +0530

    6/11/2023

---
+++ Practice_string.py
@@ -0,0 +1,82 @@
+
+"""
+What is string?
+  Strings are immutable data type. a string ia a sequence of characters or a sequence of elements,
+  which can be a alphabet, alpha numeric or it can be combination of all two
+String is enclosed between " or '
+
+Note:string is immutable since it creates memory space only for the elements defined.
+"""
+
+empty_string = ""
+str_a = "python"
+str_n = "1234"
+str_an = "python3.9"
+
+len_str = len(str_an)
+print("the length of string is {}".format(len_str))
+
+type_str = type(str_an)
+print("the type of variable str_an is {}".format(type_str))
+
+# indexing
+
+str_x = "python"
+len_str = len(str_x)
+print("the length of string is {}".format(len_str))
+strx_zeroeth_ind = str_x[0]
+print("zeroeth index value of a strx is {}".format(strx_zeroeth_ind))
+strx_first_ind = str_x[1]
+print("first index value of strx is {}".format(strx_first_ind))
+strx_second_ind = str_x[2]
+print("second index value of strx is {}".format(strx_second_ind))
+strx_third_ind = str_x[3]
+print("third index value of strx is {}".format(strx_third_ind))
+strx_fourth_ind = str_x[4]
+print("fourth index value of strx is {}".format(strx_fourth_ind))
+strx_fifth_ind = str_x[5]
+print("fifth index value of strx is {}".format(strx_fifth_ind))
+
+str_y = "welcome to class"
+len_str = len(str_y)
+print("the length of string is {}".format(len_str))
+type_str = type(str_y)
+print("the type of variable str_y is {}".format(type_str))
+stry_zeroeth_ind = str_y[0]
+print("zeroeth index value of a stry is {}".format(stry_zeroeth_ind))
+stry_first_ind = str_y[1]
+print("first index value of a stry is {}".format(stry_first_ind))
+stry_second_ind = str_y[2]
+print("second index value of a stry is {}".format(stry_second_ind))
+stry_third_ind = str_y[3]
+print("third index value of a stry is {}".format(stry_third_ind))
+stry_fourth_ind = str_y[4]
+print("fourth index value of a stry is {}".format(stry_fourth_ind))
+stry_fifth_ind = str_y[5]
+print("fifth index value of a stry is {}".format(stry_fifth_ind))
+stry_sixth_ind = str_y[6]
+print("sixth index value of a stry is {}".format(stry_sixth_ind))
+stry_seventh_ind = str_y[7]
+print("seventh index value of a stry is {}".format(stry_seventh_ind))
+stry_eighth_ind = str_y[8]
+print("eighth index value of a stry is {}".format(stry_eighth_ind))
+stry_ninth_ind = str_y[9]
+print("ninth index value of a stry is {}".format(stry_ninth_ind))
+stry_tenth_ind = str_y[10]
+print("tenth index value of a stry is {}".format(stry_tenth_ind))
+stry_eleventh_ind = str_y[11]
+print("eleventh index value of a stry is {}".format(stry_eleventh_ind))
+stry_twelveth_ind = str_y[12]
+print("twelveth index value of a stry is {}".format(stry_twelveth_ind))
+stry_thirteenth_ind = str_y[13]
+print("thirteenth index value of a stry is {}".format(stry_thirteenth_ind))
+stry_fourteenth_ind = str_y[14]
+print("fourteenth index value of a stry is {}".format(stry_fourteenth_ind))
+stry_fifteenth_ind = str_y[15]
+print("fifteenth index value of a stry is {}".format(stry_fifteenth_ind))
+
+
+
+
+
+
---
+++ practice_list.py
@@ -0,0 +1,102 @@
+
+"""
+what is string?
+list is a group of elements which can be alphabet,numeric or alpha numeric (or) combination of all 3
+list is mutable
+list is enclosed between []
+
+Note:list is mutable since it creates extra memory space it is defined
+"""
+
+lst_a = ["a","b",1,3]
+emp_lst = []                                # one kind of defining empty list
+print("one kind of defining empty list is {}".format(emp_lst))
+emp_lst = []                                 # another kind of defining empty list
+print("another kind of defining empty list is {}".format(emp_lst))
+
+# to find length of list
+lst_x = ["a","b","c",1,2]
+len_lstx = len(lst_x)
+print("length of list x is {}".format(len_lstx))
+
+# to find type
+print("the type of the variable is {}".format(type(lst_x)))
+
+lst_y = ["1","2","python",123,"@abc"]
+len_lsty = len(lst_y)
+print("the length of list is {}".format(len_lsty))
+
+# indexing of lst_y
+ind_lsty = lst_y[0]
+print("the zeroeth index of lst_y is {}".format(ind_lsty))
+ind_lsty = lst_y[1]
+print("the first index of lst_y is {}".format(ind_lsty))
+ind_lsty = lst_y[2]
+print("the second index of list_y is {}".format(ind_lsty))
+ind_lsty = lst_y[3]
+print("the third index of list_y is {}".format(ind_lsty))
+ind_lsty = lst_y[4]
+print("the fourth index of list_y is {}".format(ind_lsty))
+
+# to print h from python which is an item of lst_y
+lst_python = lst_y[2]
+print("the value is {}".format(lst_python))
+lsty_h = lst_python[3]
+print("the value is {}".format(lsty_h))
+lst_y_h = lst_y[2][3]
+print("another way of indexing {}".format(lst_y_h))
+lst_y[3] = 456
+print("the lst_y after changing the value is {}".format(lst_y))
+
+"""
+append: * we can add only 1 element at a time using append command
+        * append is faster since its adds only one element and there is no internal iterations performed
+        * time complexity is less
+"""
+# append - we can add only  single item
+lst_y.append(123)
+print("the value of lst_y after appending is {}".format(lst_y))
+
+"""
+extend: * we can add n number of elements at a time using extend command
+        * extend is slow compared to append since it is iterates through out the elements it needs to be added
+        * time complexity is more
+"""
+# extend - we can add multiple items at a time
+lst_y.extend(["a","b"])
+print("the lst_y after extending is {}".format(lst_y))
+lst_y.extend([["3","4"]])
+print("the lst_y after extending is {}".format(lst_y))
+
+lst_c = ["abc","789","box","1"]
+len_lstc = len(lst_c)
+print("the length of list is {}".format(len_lstc))
+
+# indexing of lst_c
+ind_lstc = lst_c[0]
+print("the zeroeth index of lst_c is {}".format(ind_lstc))
+ind_lstc = lst_c[1]
+print("the first index of lst_c is {}".format(ind_lstc))
+ind_lstc = lst_c[2]
+print("the second index of lst_c is {}".format(ind_lstc))
+ind_lstc = lst_c[3]
+print("the third index of lst_c is {}".format(ind_lstc))
+
+# to print x from box which as item of lst_c
+lst_box = lst_c[2]
+print("the value is {}".format(lst_box))
+lstc_x = lst_box[2]
+print("the value is {}".format(lstc_x))
+lst_c_x = lst_c[2][2]
+print("another way of indexing {}".format(lst_c_x))
+
+lst_c[3] = 5
+print("the lst_c after changing the value is {}".format(lst_c))
+
+lst_c.append(123)
+print("the value of lst_c after appending is {}".format(lst_c))
+
+lst_c.extend(["a","b"])
+print("the value of lst_c after extend is {}".format(lst_c))
+lst_c.extend([["1","2"]])
+print("the value of lst_c after extend is {}".format(lst_c))
